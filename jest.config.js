602a56dae1d1c9252a9f9ee0f73d3e85b235bf9a
Michael Haufe <tno@thenewobjective.com>
2020-12-20 15:33:27 -0600

    updated configuration

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,15 @@
+/*!
+ * @license
+ * Copyright (C) 2020 Michael L Haufe
+ * SPDX-License-Identifier: AGPL-3.0-only
+ * @see <https://spdx.org/licenses/AGPL-3.0-only.html>
+ */
+
 module.exports = {
-    "reporters": [
-      "default",
-      ["jest-junit", { "outputDirectory": "./coverage" }]
-    ],
-    "testEnvironment": "node",
-    "transform": { 
-      '^.+\\.ts$': 'ts-jest'
-    }
-}
+  reporters: [
+    'default',
+    ['jest-junit', { outputDirectory: './coverage' }]
+  ],
+  preset: 'ts-jest',
+  testEnvironment: 'node'
+};
